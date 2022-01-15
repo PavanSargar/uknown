cb8fef122db9523ea8b14a5368283bb876913a7f
Pavan Sargar <pavansargar36@gmail.com>
2022-01-15 23:40:43 +0530

    inital commit

--- src/components/sections/Faq.jsx
+++ src/components/sections/Faq.jsx
@@ -1,24 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 import { Container, Accordion } from "react-bootstrap";
 import styles from "./Faq.module.css";
 
-// dummy faqs
-import { faq } from "../../data";
-
 function Faq() {
+  const [faqs, setFaqs] = useState([]);
+
+  useEffect(async () => {
+    await Axios.get("https://crud-solana.herokuapp.com/faqs")
+      .then((response) => setFaqs(response.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <Container id="faqs" className={styles.container}>
       <h2>FAQ</h2>
 
       <Container className={styles["accordion-container"]}>
         <Accordion>
-          {faq.map((data) => (
-            <Accordion.Item className={styles.accordion} key={data.key} eventKey={data.key}>
+          {faqs.map((data) => (
+            <Accordion.Item className={styles.accordion} key={data._id} eventKey={data._id}>
               <Accordion.Header className={styles["accordion-title"]}>
-                <h4>{data.title}</h4>
+                <h4>{data.question}</h4>
               </Accordion.Header>
               <Accordion.Body className={styles["accordion-text"]}>
-                <p>{data.text}</p>
+                <p>{data.answer}</p>
               </Accordion.Body>
             </Accordion.Item>
           ))}
